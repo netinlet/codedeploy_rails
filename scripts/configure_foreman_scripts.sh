f1733bec9cc265fdbd2e4ac2f70718cc1adef723
Doug Bryant <doug@netinlet.com>
2016-06-16 13:00:10 -0400

    output cwd - thought working directory was the app directory

--- scripts/configure_foreman_scripts.sh
+++ scripts/configure_foreman_scripts.sh
@@ -16,4 +16,7 @@ foreman export --port 3000 --user=ec2-user supervisord tmp/foreman_export
 
 sudo mkdir -p /var/log/app
 sudo mkdir -p /etc/supervisor.d
+
+pwd
+
 sudo cp tmp/foreman_export/*.conf /etc/supervisor.d/
